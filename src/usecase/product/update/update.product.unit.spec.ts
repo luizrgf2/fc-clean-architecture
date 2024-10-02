b7ae81f709d2b6292f22b24b691e8b16c5fe0d54
Luiz Ricardo <luizrgfelipe@gmail.com>
2024-10-01 23:43:06 -0300

    🧹 CHORE: remove class implementation from test

--- src/usecase/product/update/update.product.unit.spec.ts
+++ src/usecase/product/update/update.product.unit.spec.ts
@@ -1,23 +1,7 @@
 import Product from "../../../domain/product/entity/product";
-import ProductRepositoryInterface from "../../../domain/product/repository/product-repository.interface";
-import { InputUpdateProductDto, OutputUpdateProductDto } from "./update.product.dto";
+import { UpdateProductUseCase } from "./update.product.usecase";
 
-class UpdateProductUseCase {
-    constructor(private readonly productRepository: ProductRepositoryInterface) {}
 
-    async execute(input: InputUpdateProductDto): Promise<OutputUpdateProductDto> {
-        const productOrError = await this.productRepository.find(input.id)
-        productOrError.changeName(input.name)
-        productOrError.changePrice(input.price)
-
-        await this.productRepository.update(productOrError)
-        return {
-            id: productOrError.id,
-            name: productOrError.name,
-            price: productOrError.price
-        }
-    }
-}
 
 describe('Unit tests to update product', ()=>{
 
