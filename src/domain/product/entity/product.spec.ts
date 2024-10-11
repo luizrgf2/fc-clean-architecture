01a007423bf14820656e7dfe7c8aa03ca79467d4
Luiz Ricardo <luizrgfelipe@gmail.com>
2024-10-11 00:39:21 -0300

    📦 FEAT: add notification to product

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -3,20 +3,26 @@ import Product from "./product";
 describe("Product unit tests", () => {
   it("should throw error when id is empty", () => {
     expect(() => {
-      const product = new Product("", "Product 1", 100);
-    }).toThrowError("Id is required");
+      new Product("", "Product 1", 100);
+    }).toThrowError("Product: Id is required");
   });
 
   it("should throw error when name is empty", () => {
     expect(() => {
-      const product = new Product("123", "", 100);
-    }).toThrowError("Name is required");
+      new Product("123", "", 100);
+    }).toThrowError("Product: Name is required");
   });
 
   it("should throw error when price is less than zero", () => {
     expect(() => {
-      const product = new Product("123", "Name", -1);
-    }).toThrowError("Price must be greater than zero");
+      new Product("123", "Name", -1);
+    }).toThrowError("Product: Price must be greater than zero");
+  });
+
+  it("should throw error when price is less than zero and name is empty and id is empty", () => {
+    expect(() => {
+      new Product("", "", -1);
+    }).toThrowError("Product: Id is required,Product: Name is required,Product: Price must be greater than zero");
   });
 
   it("should change name", () => {
